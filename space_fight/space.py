60ac9492c558df6c47cbf0dd6dd9fa00cf2c0fe4
Gustavo Henrique Spiess <gustavo.spiess@philips.com>
2020-06-18 22:14:10 -0300

    mock code to confirm pytest cofig

---
+++ space_fight/space.py
@@ -0,0 +1,9 @@
+import typing as tp
+import dataclasses
+
+
+@dataclasses.dataclass
+class position():
+    x: int
+    y: int
+    z: int
---
+++ space_fight/tests/test_base_assertion.py
@@ -0,0 +1,5 @@
+from .. import space
+
+def test_base():
+    sp = space.position(0, 0, 0)
+    assert True
