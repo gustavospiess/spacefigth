160eee2ab71cf172ff1ab11f965be96d505d8752
Gustavo Henrique Spiess <gustavo.spiess@philips.com>
2020-06-22 22:08:31 -0300

    Frame implementation

--- space_fight/space.py
+++ space_fight/space.py
@@ -1,9 +1,123 @@
 import typing as tp
 import dataclasses
+import random as rd
 
 
-@dataclasses.dataclass
-class position():
+class Position(tp.NamedTuple):
     x: int
     y: int
     z: int
+
+    @classmethod
+    def randomPosition(cls, bounds = 100):
+        """Generates a new position inside the bounds"""
+        return cls(*[rd.randint(-1*bounds, bounds) for _ in range(3)])
+
+
+@dataclasses.dataclass
+class SensorState():
+    """TODO"""
+    fuel:float = dataclasses.field(default=0.0, compare=True, init=True)
+
+
+@dataclasses.dataclass
+class ActionSet():
+    """TODO"""
+    pass
+
+
+onSensorEvent = tp.List[tp.Callable[[SensorState], tp.NoReturn]];
+onActionEvent = tp.List[tp.Callable[[], ActionSet]];
+
+
+class NotEnouthPlayers(Exception):
+    pass
+
+
+class _Player(object):
+    """Player interface"""
+    def __init__(self):
+        self._onSensor: tp.Set[onSensorEvent] = set()
+        self._onAction: tp.Set[onActionEvent] = set()
+        self._position: 'Position' = None
+        self._fuel: float = 0.0
+
+    @property
+    def onSensor(self) -> tp.FrozenSet[onSensorEvent]:
+        """The onSensor event set."""
+        return frozenset(self._onSensor)
+
+    @property
+    def onAction(self) -> tp.FrozenSet[onActionEvent]:
+        """The onSensor event set."""
+        return frozenset(self._onAction)
+    
+    @property
+    def position(self) -> Position:
+        return self._position
+
+    @property
+    def fuel(self) -> float:
+        return self._fuel
+
+    def appendOnSensor(self, sensor: onSensorEvent) -> None:
+        """Add event to on sensor call"""
+        self._onSensor.add(sensor)
+
+    def appendOnAction(self, action: onActionEvent) -> None:
+        """Add event to on action call"""
+        self._onAction.add(action)
+
+class Match(object):
+    """A handler for a single match"""
+    def __init__(self, player_set: tp.Set[_Player]):
+        self._players = player_set
+        self._time = 0
+
+        self._init_player_pos()
+
+    def _init_player_pos(self):
+        position_set = set()
+        while len(position_set) < len(self.players):
+            pos = Position.randomPosition()
+            position_set.add(pos)
+        for player in self.players:
+            player._position = position_set.pop()
+
+    def ticTimer(self):
+        for player in self._players:
+            sensor_state = SensorState(fuel = player.fuel);
+            for sensor in player.onSensor:
+                sensor(sensor_state)
+            actionSet = None
+            for action in player.onAction:
+                actionSet = action()
+        self._time += 1
+
+    @property
+    def time(self):
+        """global timer to the match"""
+        return self._time
+
+    @property
+    def players(self) -> tp.Set[_Player]:
+        """Return player list of the match"""
+        return self._players
+
+
+class MatchBuilder(object):
+    """Builder to preset the Match"""
+
+    def __init__(self):
+        self._players = list()
+        
+    def addPlayer(self) -> '_Player':
+        """Generates new player in the match and return it"""
+        new_player = _Player()
+        self._players.append(new_player)
+        return new_player
+        
+    def start(self):
+        if len(self._players) < 2:
+            raise NotEnouthPlayers()
+        return Match(self._players)
--- space_fight/tests/test_base_assertion.py
+++ space_fight/tests/test_base_assertion.py
@@ -1,5 +1,103 @@
+import pytest # type: ignore
 from .. import space
 
-def test_base():
-    sp = space.position(0, 0, 0)
-    assert True
+def set_up_match():
+    match_builer = space.MatchBuilder()
+    player_a = match_builer.addPlayer()
+    player_b = match_builer.addPlayer()
+    match = match_builer.start()
+    return (player_a, player_b, match)
+
+def test_new_player_is_in_game():
+    (player_a, player_b, match) = set_up_match()
+    assert player_a in match.players
+    assert player_b in match.players
+
+
+def test_new_player_has_no_events():
+    match_builer = space.MatchBuilder()
+    player = match_builer.addPlayer()
+    assert len(player.onSensor) == 0
+    assert len(player.onAction) == 0
+
+
+def test_player_keeps_events():
+    (player_a, player_b, match) = set_up_match()
+    sensor = lambda state: None
+    action = lambda: None
+
+    player_a.appendOnSensor(sensor)
+    player_a.appendOnAction(action)
+
+    assert sensor in player_a.onSensor
+    assert action in player_a.onAction
+
+
+def test_match_cant_start_with_less_than_two_players():
+    match_builer = space.MatchBuilder()
+
+    with (pytest.raises(space.NotEnouthPlayers)):
+        match_builer.start()
+
+    match_builer.addPlayer()
+    with (pytest.raises(space.NotEnouthPlayers)):
+        match_builer.start()
+
+    match_builer.addPlayer()
+    match_builer.start()
+
+
+def test_before_start_match_players_have_no_position():
+    match_builer = space.MatchBuilder()
+    player_a = match_builer.addPlayer()
+    player_b = match_builer.addPlayer()
+    assert player_a.position == None
+    assert player_b.position == None
+
+
+def test_after_start_match_players_have_diferente_positions():
+    (player_a, player_b, match) = set_up_match()
+    assert player_a.position != player_b.position
+
+def test_started_match_inreases_time_by_one_after_tic():
+    (player_a, player_b, match) = set_up_match()
+    time_pre_tic = match.time
+    match.ticTimer()
+    time_pos_tic = match.time
+    assert time_pre_tic + 1 == time_pos_tic
+
+
+def test_execute_action_event_on_tic():
+    (player_a, player_b, match) = set_up_match()
+
+    called = { 'on_sensor': False, 'on_action': False, }
+    def on_sensor_call_back(state: space.SensorState):
+        called['on_sensor'] = True
+
+    called_on_action = False
+    def on_action_call_back() -> space.ActionSet:
+        called['on_action'] = True
+        return space.ActionSet()
+
+    player_a.appendOnSensor(on_sensor_call_back)
+    player_a.appendOnAction(on_action_call_back)
+
+    assert not called['on_sensor']
+    assert not called['on_action']
+
+    match.ticTimer()
+
+    assert called['on_sensor']
+    assert called['on_action']
+
+
+def test_on_sensor_informs_current_fuel():
+    (player_a, player_b, match) = set_up_match()
+
+    f = 10.5
+
+    def on_sensor_call_back(state: space.SensorState):
+        assert state.fuel == f
+    player_a.appendOnSensor(on_sensor_call_back)
+    player_a._fuel = f
+    match.ticTimer()
